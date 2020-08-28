7edeb42e9a2c63551ff1a5cf9a1d66baed0c7729
Getof <bohonovdenis1980@gmail.com>
2020-08-28 19:41:58 +0700

    up

---
+++ app/src/main/java/ru/ecom42/taxismartrider/Common/Common.java
@@ -0,0 +1,8 @@
+package ru.ecom42.taxismartrider.Common;
+
+import ru.ecom42.taxismartrider.Model.RiderModel;
+
+public class Common {
+    public static final String RIDER_INFO_REFENCE = "Riders";
+    public static RiderModel currentRider;
+}
---
+++ app/src/main/java/ru/ecom42/taxismartrider/Model/RiderModel.java
@@ -0,0 +1,32 @@
+package ru.ecom42.taxismartrider.Model;
+
+public class RiderModel {
+    private String firstName, LastName, phoneNumber;
+
+    public RiderModel() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
